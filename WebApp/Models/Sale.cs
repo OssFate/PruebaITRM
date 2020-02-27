f41b97ad83a202c67357835f4102fd2810db1a5f
OssFate <mantis1512@gmail.com>
2020-02-26 19:45:10 -0500

    Better details on Sales UI

--- WebApp/Models/Sale.cs
+++ WebApp/Models/Sale.cs
@@ -13,7 +13,9 @@ namespace WebApp.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public Client SaleClient { get; set; }
+        public string SaleClientId { get; set; }
         public Vehicle SaleVehicle { get; set; }
+        public string SaleVehicleId { get; set; }
 
         public DateTime SaleDate { get; set; }
         public bool PaidFull { get; set; }
